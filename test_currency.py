a0735208fbe483d1706d60d31f6f970367101ebe
Bob Belderbos <bbelderbos@users.noreply.github.com>
2016-06-21 10:44:15 +0200

    chapter 3

--- test_currency.py
+++ test_currency.py
@@ -5,11 +5,15 @@ import unittest
 class testCurrency(unittest.TestCase):
   
   def testMultiplication(self):
-    five = Dollar(5);
-    product = five.times(2);
-    self.assertEquals(10, product.amount);
-    product = five.times(3);
-    self.assertEquals(15, product.amount);
+    five = Dollar(5)
+    product = five.times(2)
+    self.assertEquals(10, product.amount)
+    product = five.times(3)
+    self.assertEquals(15, product.amount)
+
+  def testEquality(self):
+    self.assertTrue(Dollar(5).equals(Dollar(5)))
+    self.assertFalse(Dollar(5).equals(Dollar(6)))
 
 if __name__ == "__main__":
   unittest.main()
